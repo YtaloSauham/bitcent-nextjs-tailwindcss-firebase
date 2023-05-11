6496110c8f9ee2625f19955c6f9417906bc2c130
YtaloSauham <ytalo.sauhan@hotmail.com>
2023-05-11 20:45:52 -0300

    image responsive and hero finished

---
+++ src/components/landing/common/ResponsiveImage.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+interface ResponsiveImageProps {
+    image: any
+    className?: string
+}
+
+export default function ResponsiveImage(props: ResponsiveImageProps) {
+    return (
+        <Image
+            src={props.image}
+            alt="Imagem"
+            className={`
+                w-[350px] h-[365px]
+                lg:w-[550px] lg:h-[365px]
+                rounded-xl object-cover
+                ${props.className ?? ''}
+            `}
+        />
+    )
+}
--- src/components/landing/hero/index.tsx
+++ src/components/landing/hero/index.tsx
@@ -1,14 +1,17 @@
+import ResponsiveImage from "../common/ResponsiveImage";
 import Layout from "../layout";
 import Slogan from "./Slogan";
+import principal from "../../../../public/principal.jpg"
 
 export default function Hero() {
     return (
         <Layout>
             <div className={`
-            flex items-center
+            flex items-center justify-around
             h-[500px]
             `}>
                 <Slogan />
+                <ResponsiveImage image={principal} className="rotate-3  mix-blend-plus-lighter hidden md:inline" />
             </div>
 
         </Layout>
